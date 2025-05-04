09c4f263979012d3481a6cb2cf9f75bf0be0768a
Muhammad Reda <muhammadreda97@gmail.com>
2025-05-04 10:11:22 +0300

    Refactor MixedGroupConditionException constructor to use a fixed message

--- src/Exceptions/Conditions/MixedGroupConditionException.php
+++ src/Exceptions/Conditions/MixedGroupConditionException.php
@@ -6,8 +6,8 @@ use Exception;
 
 class MixedGroupConditionException extends Exception
 {
-    public function __construct(string $message = "Group Condition Should Be Aggregation's/ non-aggregation's, can not be mixed.")
+    public function __construct()
     {
-        parent::__construct($message);
+        parent::__construct("Group Condition Should Be Aggregation's/ non-aggregation's, can not be mixed.");
     }
 }
